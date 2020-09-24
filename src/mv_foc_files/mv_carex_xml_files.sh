48ab6857d0648a0a4aa0488a4e8ed08ba354a681
Jocelyn Pender <jpend049@uottawa.ca>
2020-09-24 11:56:01 -0400

    save the commands used in the terminal to copy over the foc files

---
+++ src/mv_foc_files/mv_carex_xml_files.sh
@@ -0,0 +1,4 @@
+
+cd /Users/jocelynpender/fc/fochina/FoC2-5.13-25/FoCV23
+
+cat /Users/jocelynpender/carex-climate-morpho/data/interim/FoCV23_treatment_list.txt | xargs -J % cp % /Users/jocelynpender/carex-climate-morpho/data/external
