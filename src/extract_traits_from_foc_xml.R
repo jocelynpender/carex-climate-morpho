a8ed877eac922e6386e99f92cdb9cf30258866eb
Jocelyn Pender <jpend049@uottawa.ca>
2020-09-24 14:22:24 -0400

    work on FoC files with python rather than R. 1st step: delete the R file

--- src/extract_traits_from_foc_xml.R
+++
@@ -1,4 +0,0 @@
-# FoC plan
-# use the structured XML files to grab Carex descriptions: easier and faster than web scraping
-# Dump the V23 files into this repo so that the entire project is self-contained
-# How to add only the Carex files/???
