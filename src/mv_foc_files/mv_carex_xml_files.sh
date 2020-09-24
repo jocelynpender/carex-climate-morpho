101734ee555423e78785a01a0ef7578f73346085
Jocelyn Pender <jpend049@uottawa.ca>
2020-09-24 12:03:43 -0400

    fix paths to move parsed carex files

--- src/mv_foc_files/mv_carex_xml_files.sh
+++ src/mv_foc_files/mv_carex_xml_files.sh
@@ -1,4 +1,4 @@
 
-cd /Users/jocelynpender/fc/fochina/FoC2-5.13-25/FoCV23
+cd /Users/jocelynpender/fc/fochina/FoCV23/target/final/
 
-cat /Users/jocelynpender/carex-climate-morpho/data/interim/FoCV23_treatment_list.txt | xargs -J % cp % /Users/jocelynpender/carex-climate-morpho/data/external
+cat /Users/jocelynpender/carex-climate-morpho/data/interim/FoCV23_treatment_list.txt | xargs -J % cp % /Users/jocelynpender/carex-climate-morpho/data/external/FoCV23
