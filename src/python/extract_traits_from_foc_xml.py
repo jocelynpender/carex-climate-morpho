fba14a205ba491a40c668e10d13db2407e02beaa
Jocelyn Pender <jpend049@uottawa.ca>
2020-09-25 13:04:37 -0400

    begin to pull out character ranges

--- src/python/extract_traits_from_foc_xml.py
+++ src/python/extract_traits_from_foc_xml.py
@@ -27,20 +27,38 @@ def extract_structures(morphology):
         structure_all = statement.findall('structure')
         # text_verbatim = statement.find('text').text
         # print("* " + text_verbatim + " *")
-        rows.append(pd.Series(extract_structure_names(structure_all)))
-
-    out_df = pd.DataFrame(rows)
+        rows.append(extract_structure_names(structure_all))
+        #print(extract_structure_names(structure_all))
+    #out_df = pd.DataFrame(rows)
+    #print(rows)
     return rows
 
 
 def extract_structure_names(structure_all):
     for structure_element in structure_all:
         structure = structure_element.attrib['name']
-        # parse_all = structure_element.getchildren()
-        return(structure)
+        character_all_data = extract_characters(structure_element, structure)
+        return(character_all_data)
+
+
+def extract_characters(structure_element, structure):
+    character_all = structure_element.findall('character')
+    character_all_data = []
+    for character_element in character_all:
+        property_name = structure + '_' + character_element.attrib['name']
+        property_from = extract_from_to_attribs(character_element, 'from')
+        property_to = extract_from_to_attribs(character_element, 'to')
+        print([property_name, property_from, property_to])
+        character_all_data.append([property_name, property_from, property_to])
+    return character_all_data
+
+
+def extract_from_to_attribs(character_element, from_or_to):
+    if 'from' in character_element.attrib:
+        return character_element.attrib[from_or_to] + character_element.attrib[from_or_to + '_unit']
 
 
 file_name = "../../data/external/FoCV23/1001.xml"
 parsed_xml = parse(file_name)
 morphology = extract_morphology(parsed_xml)
-test = extract_structures(morphology)
+rows = extract_structures(morphology)
