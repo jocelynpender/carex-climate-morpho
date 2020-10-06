264a1805ac53fe1e0a07b1bcb991bf28618e7604
Jocelyn Pender <jpend049@uottawa.ca>
2020-10-06 14:55:13 -0400

    add FNA carex XML files and treatment list to repo for XML data extract

--- src/mv_foc_files/mv_carex_xml_files.sh
+++ src/mv_foc_files/mv_carex_xml_files.sh
@@ -1,4 +1,9 @@
-
+# foc
 cd /Users/jocelynpender/fc/fochina/FoCV23/target/final/
-
 cat /Users/jocelynpender/carex-climate-morpho/data/interim/FoCV23_treatment_list.txt | xargs -J % cp % /Users/jocelynpender/carex-climate-morpho/data/external/FoCV23
+
+
+#fna
+cd /Users/jocelynpender/fna/fna-fine-grained-xml/V23/
+cat /Users/jocelynpender/carex-climate-morpho/data/interim/FNAV23_treatment_list.txt | xargs -J % cp % /Users/jocelynpender/carex-climate-morpho/data/external/FNAV23
+
