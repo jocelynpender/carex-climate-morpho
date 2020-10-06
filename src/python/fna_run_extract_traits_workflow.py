286145078e52ed73db8a0d43a79d4dc4aa80e2b2
Jocelyn Pender <jpend049@uottawa.ca>
2020-10-06 15:24:33 -0400

    run xml extraction workflow on fna data

---
+++ src/python/fna_run_extract_traits_workflow.py
@@ -0,0 +1,54 @@
+import glob
+import pandas as pd
+
+from src.python.collapse_traits_from_foc_xml import collapse_traits
+from src.python.extract_traits_from_xml import parse, extract_morphology, extract_structures
+
+# file_name = "../../data/external/FoCV23/1001.xml"
+directory = "../../data/external/FNAV23/*.xml"
+carex_traits_data_frame = pd.DataFrame([], columns=['property_name', 'from', 'to', 'species_name', 'file_name'])
+
+
+def format_taxon_id(parsed_xml):
+    taxon_id = parsed_xml.find('taxon_identification[@status=\"ACCEPTED\"]')
+    genus = taxon_id.find('taxon_name[@rank=\"genus\"]')
+    species = taxon_id.find('taxon_name[@rank=\"species\"]')
+    return genus.text + '_' + species.text
+
+
+for file_name in glob.glob(directory):
+    print(file_name)
+    parsed_xml = parse(file_name)
+    species_name = format_taxon_id(parsed_xml)
+    morphology = extract_morphology(parsed_xml, morphology_node='description[@type=\"morphology\"]')
+    if morphology is not None:
+        all_structures_data_frame = extract_structures(morphology, structure_node='biological_entity[@type=\"structure\"]')
+        all_structures_data_frame = all_structures_data_frame.assign(species_name=species_name)
+        all_structures_data_frame = all_structures_data_frame.assign(file_name=file_name)
+        carex_traits_data_frame = carex_traits_data_frame.append(all_structures_data_frame)
+
+# sum is used to unnest the reps, https://stackoverflow.com/questions/11860476/how-to-unnest-a-nested-list
+collapse_property_coding = pd.DataFrame({'coded_property_name': sum([['maximum_plant_height'] * 8,
+                                                                   ['maximum_leaf_width'] * 4,
+                                                                   ['maximum_inflorescence_length'] * 4,
+                                                                   ['maximum_inflorescence_width'] * 2,
+                                                                    ['maximum_fruit_length'] * 2,
+                                                                    ['maximum_fruit_width'] * 2], [])
+                                       , 'property_name': ['culm_size', 'culm_height', 'culm_atypical_size',
+                                                           'culm_height_or_length', 'culm_length',
+                                                           'culm_some_measurement', 'culm_leaf_some_measurement',
+                                                           'angle_some_measurement',
+                                                           'blade_size', 'blade_width', 'leaf_size', 'leaf_width',
+                                                           'inflorescence_height_or_length', 'inflorescence_length',
+                                                           'inflorescence_some_measurement', 'inflorescence_atypical_length',
+                                                           'inflorescence_atypical_width', 'inflorescence_width',
+                                                           'achene_atypical_length', 'achene_length',
+                                                           'achene_atypical_width', 'achene_width']})
+
+carex_traits_collapsed = collapse_traits(carex_traits_data_frame, collapse_property_coding)
+
+# trim semi-colons and nan
+carex_traits_collapsed.collapsed_data = carex_traits_collapsed.collapsed_data.apply(lambda x: x.replace(";nan", "").strip(";nan"))
+
+carex_traits_data_frame.to_csv("../../data/processed/fna/fna_carex_traits_data_frame.csv")
+carex_traits_collapsed.to_csv("../../data/processed/fna/fna_carex_traits_collapsed.csv")
