f1e0acb694c1a0be983abd5da20dc8bc07b0b7f8
Jocelyn Pender <jpend049@uottawa.ca>
2020-12-16 15:54:13 -0500

    WIP add constraint to the collapsed csv

--- src/python/collapse_traits_from_xml.py
+++ src/python/collapse_traits_from_xml.py
@@ -18,6 +18,8 @@ def collapse_traits(property_data_frame, coded_property_name_df, sep_property_da
     :return: coded_collapsed: cleaned, aggregated pandas dataframe with species_name, coded_property_name and
     pasted values
     """
+    default_cols = ['species_name', 'coded_property_name', 'collapsed_data_value', 'property_constraint']
+
     coded_data_frame = property_data_frame.set_index('property_name'). \
         join(coded_property_name_df.set_index('property_name'))  # add coded_property name as a column in a new data
     # frame
@@ -32,13 +34,19 @@ def collapse_traits(property_data_frame, coded_property_name_df, sep_property_da
 
     coded_data_frame['collapsed_data_value'] = coded_data_frame.value.astype(str)
 
-    collapsed_data_cols.extend(['coded_property_name', 'species_name', 'collapsed_data_value'])
+    collapsed_data_cols.extend(default_cols)
     coded_data_frame = coded_data_frame[collapsed_data_cols]  # filter the data frame to only the collapsed data values
 
     coded_value = coded_data_frame[coded_data_frame.collapsed_data_value != "nan"] \
-        [['species_name', 'coded_property_name', 'collapsed_data_value']]  # remove string 'nan' values
-    coded_value = coded_value.groupby(['species_name', 'coded_property_name']) \
+        [default_cols]  # remove string 'nan' values
+    coded_value = coded_value.groupby(['species_name', 'coded_property_name', 'property_constraint']) \
         ['collapsed_data_value'].apply(sep_property_data.join).reset_index()
+
+    coded_data_frame['property_constraint'] = coded_data_frame.property_constraint.astype(str)
+    coded_property_constraint = coded_data_frame[coded_data_frame.property_constraint != "nan"] \
+        [default_cols]  # remove string 'nan' values
+    coded_property_constraint = coded_property_constraint.groupby(['species_name', 'coded_property_name', 'property_constraint']) \
+        ['property_constraint'].apply(sep_property_data.join).reset_index()
 
     if include_from:
         coded_maximum = coded_data_frame[coded_data_frame.collapsed_data_maximum != "nan"] \
@@ -58,5 +66,6 @@ def collapse_traits(property_data_frame, coded_property_name_df, sep_property_da
         # per new property name, including multiples, if they exist.
 
     coded_collapsed = coded_collapsed.merge(coded_value, how='outer')
+    coded_collapsed = coded_collapsed.merge(coded_property_constraint, how='outer')
 
     return coded_collapsed
